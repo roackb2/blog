7db8632c5fcdee638d89d6e9d6f89f7c56fc92e1
Jay/Fienna Liang <roackb2@gmail.com>
2023-03-12 17:21:34 +0800

    Add new article page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# The Article model
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
